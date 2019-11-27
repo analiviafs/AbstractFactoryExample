b3e478edfd2f86cc72cf3ce025af79a747f07e15
analiviafs <ana.livia@dce.ufpb.br>
2019-11-27 11:43:28 -0300

    Add files via upload
    Exercício da aula de APS sobre Abstract Factory

---
+++ abstractMethod/src/abstractMethod/FabricaAndroid.java
@@ -0,0 +1,5 @@
+package abstractMethod;
+
+public class FabricaAndroid {
+
+}
---
+++ abstractMethod/src/abstractMethod/telaProjetoTest.java
@@ -0,0 +1,14 @@
+package abstractMethod;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class telaProjetoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
